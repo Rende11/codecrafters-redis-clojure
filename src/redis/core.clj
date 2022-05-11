e142ca026cfe5041c5361899520af82f43b5743d
Rende11 <artamonovn@gmail.com>
2022-05-11 17:59:29 +0300

    Pass 1st stage

--- src/redis/core.clj
+++ src/redis/core.clj
@@ -34,5 +34,4 @@
   ;; You can use print statements as follows for debugging, they'll be visible when running tests.
   (println "Logs from your program will appear here!")
   ;; Uncomment this block to pass the first stage
-  ;; (serve 6379 handler)
-  )
+  (serve 6379 handler))
